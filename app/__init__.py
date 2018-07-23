00b42774c92821e6c807aec4fb5584d4d5fa4e28
kreng <glaps122@rambler.ru>
2018-07-23 17:20:31 +0300

    upgrade db

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,6 @@ apli = Flask(__name__)
 apli.config["SECRET_KEY"] = "sieuj3@Sjwq1"
 apli.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 apli.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 db = SQLAlchemy(apli)
 migrate = Migrate(apli, db)
 from app import routes, models
