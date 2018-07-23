5642d4aa8b8b8387715a101c463959c09a014f5d
kreng <glaps122@rambler.ru>
2018-07-23 15:09:06 +0300

    add db

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 from flask import Flask
 from config import *
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 apli = Flask(__name__)
 apli.config["SECRET_KEY"] = "sieuj3@Sjwq1"
-from app import routes
+apli.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+apli.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(apli)
+migrate = Migrate(apli, db)
+from app import routes, models
