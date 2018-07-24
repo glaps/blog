a237ffde3cbb5d967a49e83fa96abde35a8428b9
kreng <glaps122@rambler.ru>
2018-07-24 17:18:21 +0300

    stage 5

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,13 @@ from flask import Flask
 from config import *
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 apli = Flask(__name__)
 apli.config["SECRET_KEY"] = "sieuj3@Sjwq1"
 apli.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 apli.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(apli)
 migrate = Migrate(apli, db)
+login = LoginManager(apli)
+login.login_view = 'login'
 from app import routes, models
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import apli, db
 from app.models import User, Post
 
-@app.shell_context_processor
+@apli.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
 
