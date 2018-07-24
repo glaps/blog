239b01e90682834d5986a449453ea97eeabc9a25
kreng <glaps122@rambler.ru>
2018-07-24 08:37:06 +0300

    just

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from app import apli
+from app import apli, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
 #if __name__=="__main__":
     #apli.run(debug=True)
