3c12597de3c376900e65d6253b1084d5d2c82a57
kreng <glaps122@rambler.ru>
2018-07-30 14:37:41 +0300

    add new funk

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from flask import render_template, redirect, flash, url_for, request
-from app import apli
-from app.forms import LoginForm
+from app import apli, db
+from app.forms import LoginForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user,login_required
 from werkzeug.urls import url_parse
 from app.models import User
-
+from datetime import datetime
 
 @apli.route("/")
 @apli.route("/index")
@@ -12,6 +12,11 @@ from app.models import User
 def index():
     return render_template("index.html",title="BLOG")
 
+@apli.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @apli.route("/login", methods=["GET","POST"])
 def login():
@@ -35,3 +40,29 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@apli.route('/usr/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@apli.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
