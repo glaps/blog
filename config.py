5264d27a4000fe9bad7fbdd723a784b98e7a31a0
kreng <glaps122@rambler.ru>
2018-08-09 12:22:07 +0300

    finish3

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ class Config(object):
     LANGUAGES = ['ru', 'en', 'ua']
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = os.environ.get('ADMINS')
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
