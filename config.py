d8852ac429732525941c1c38382a6aa5c0afcb0f
kreng <glaps122@rambler.ru>
2018-08-09 12:20:47 +0300

    finsh2

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ySWDwddwdq!@dw@#ffff3454___22e2DdW1!,.w/q;[!]{@}'
